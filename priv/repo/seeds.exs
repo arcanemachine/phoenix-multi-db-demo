8c3bad37696e89f588319e8ab1fe216b93210266
Nicholas Moen <arcanemachine@tutanota.com>
2023-11-06 23:15:20 -0700

    Create DB seeds for easy-bake demo data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,40 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     MultiDbDemo.Repo.insert!(%MultiDbDemo.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias MultiDbDemo.{V1, V2}
+
+# create v1 persons
+v1_persons = [
+  %{
+    name: "Alice Anderson",
+    age: 1
+  },
+  %{
+    name: "Bob Barker",
+    age: 2
+  },
+  %{
+    name: "Carol Carter",
+    age: 3
+  }
+]
+
+Enum.each(v1_persons, fn person -> V1.Persons.create_person(person) end)
+
+# create v2 persons
+v2_persons = [
+  %{
+    first_name: "Alice",
+    last_name: "Anderson",
+    age: 1
+  },
+  %{
+    first_name: "Bob",
+    last_name: "Barker",
+    age: 2
+  },
+  %{
+    first_name: "Carol",
+    last_name: "Carter",
+    age: 3
+  }
+]
+
+Enum.each(v2_persons, fn person -> V2.Persons.create_person(person) end)
